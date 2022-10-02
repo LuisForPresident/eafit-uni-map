08f2c9882d0ee071620774898e7ceaff96e84221
Luis M. Torres-Villegas <111613017+LuisForPresident@users.noreply.github.com>
2022-10-01 19:31:55 -0500

    Feat: calculate and display ETAs

--- main.py
+++ main.py
@@ -76,7 +76,10 @@ def print_directions(stuff):
     # (src: https://hextobinary.com/unit/length/from/step/to/meter)
     distance /= 0.762
     distance = math.ceil(distance)
-    title = '\n'.join(steps) + '\n\n' + str(distance) + ' steps \nBB secs walking or CC secs biking (pending)'
+    walking_time = math.ceil(distance/1)  # hehe
+    cycling_time = math.ceil(distance/5)
+    # starting to think that another function should just return the title to this one
+    title = '\n'.join(steps) + '\n\n' + str(distance) + ' steps \n' + str(walking_time)  + ' secs walking or ' + str(cycling_time) + ' secs cycling'
     decision = pick(options, title)[1]  # Get index
     return decision
 
