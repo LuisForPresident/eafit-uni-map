59a415fe3afc02327bf526b80d7cd28292ec3cfd
Luis M. Torres-Villegas <111613017+LuisForPresident@users.noreply.github.com>
2022-10-07 16:55:56 -0500

    Style: remove internal new lines
    In favor of code readability?
    
    I'm not entirely sure this helps. But I've found it hard to distinguish where functions end.

--- dijkstra.py
+++ dijkstra.py
@@ -11,23 +11,19 @@ time_and_distance_path = 'time_and_distance.csv'
 # Return input csv file of edge list as a list
 def parse_csv_input(file_name: str):
     edge_list = []
-
     with open(file_name) as csv_file:
         parsed_csv = csv.reader(csv_file, delimiter=";")
         for edge in parsed_csv:
             edge_list.append(edge[0].split(","))
-
     return edge_list
 
 
 # Return graph dictionary with placeholder keys
 def initialize_graph_keys(edge_list: list):
     graph_dict = {}
-
     for edge in edge_list:
         if edge not in graph_dict:
             graph_dict[edge] = []
-
     return graph_dict
 
 
@@ -43,7 +39,6 @@ def calculate_edge_distances(graph_dict: dict, initial_node: str):
     least_distance_nodes = []
     distance = {edge: 'infinity' for edge in graph_dict.keys()}
     path = {}
-
     heapq.heappush(least_distance_nodes, (0, initial_node))  # Priority queue
     while least_distance_nodes:
         current_weight, current_node = heapq.heappop(least_distance_nodes)
@@ -55,7 +50,6 @@ def calculate_edge_distances(graph_dict: dict, initial_node: str):
                 if (node not in path) or (current_weight + weight
                                           < path[node][1]):
                     path[node] = (current_node, weight)
-
     return distance, path
 
 
@@ -64,7 +58,6 @@ def show_path(path: dict, initial_node: str, final_node: str):
     directions = deque()
     directions.append(final_node)  # Avoids conversion to deque from splitting str into a list
     search = path[final_node][0]
-
     while search in path:
         directions.appendleft(search)
         search = path[search][0]
@@ -78,7 +71,6 @@ def get_shortest_path_and_distance(initial_node: str, final_node: str):
     graph = initialize_graph_keys(edge_list[0])
     for edge in edge_list[1:]:
         add_graph_edges(graph, edge[0], edge[1], float(edge[2]))
-
     distance_dict = calculate_edge_distances(graph, initial_node)
     distance = int(distance_dict[0][final_node])
     path = distance_dict[1]
