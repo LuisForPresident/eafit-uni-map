f7f88625bd3bd035b8449493348e02437fe137c1
Luis M. Torres-Villegas <111613017+LuisForPresident@users.noreply.github.com>
2022-10-10 15:18:06 -0500

    Refactor: add separate functions for conversion
    For example, the string concatenation was very long.

--- results.py
+++ results.py
@@ -9,14 +9,25 @@ def print_directions(directions: deque, distance: int):
     steps = []
     for step, place in enumerate(directions, 1):
         steps.append(str(step) + '. Go to ' + place)
-    # 1 step = 0.762 meters
-    # (src: https://hextobinary.com/unit/length/from/step/to/meter)
-    distance /= 0.762
-    distance = math.ceil(distance)
-    walking_time = math.ceil(distance / 1)  # hehe
-    cycling_time = math.ceil(distance / 5)
-    # starting to think that another function should just return the title to this one
-    title = '\n'.join(steps) + '\n\n' + str(distance) + ' steps \n' + str(walking_time) + ' secs walking or ' + str(
-        cycling_time) + ' secs cycling'
-    decision = pick(options, title)[1]  # Get index
-    return decision
+    steps_str = '\n'.join(steps)
+    return steps_str
+
+
+def create_output_title(steps_str: str,
+                        number_of_steps: int,
+                        walking_time: int) -> str:
+    stats_str = str(number_of_steps) + ' steps \n' + str(walking_time) + ' secs walking'
+    final_str = steps_str + '\n\n' + stats_str
+    return final_str
+
+
+def convert_to_steps(distance_in_meters: int) -> int:
+    number_of_steps = distance_in_meters / 0.762  # Reference: 1 step = 0.762 meters
+    number_of_steps = math.ceil(number_of_steps)  # Round to integer
+    return number_of_steps
+
+
+def estimate_walking_time(distance_in_meters: int) -> int:
+    walking_time = distance_in_meters / 1  # TODO Get a better ref for the estimation
+    # TODO Ceil the result when previous todo item is ready
+    return walking_time
