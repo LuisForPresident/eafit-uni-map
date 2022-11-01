bcb7b20259883cecdfa3effc518bf525491e7974
Luis M. Torres-Villegas <111613017+LuisForPresident@users.noreply.github.com>
2022-10-31 21:56:04 -0500

    rename real edgelist of eafit's campus

--- eafit-uni-map/config.py
+++ eafit-uni-map/config.py
@@ -1,3 +1,3 @@
-GRAPH_PATH = 'eafit-uni-map/data/prototype-v2.edgelist'
+GRAPH_PATH = 'eafit-uni-map/data/eafit-campus.edgelist'
 FAVORITES_PATH = 'eafit-uni-map/data/favorites.json'
 STATS_PATH = 'eafit-uni-map/data/time_and_distance.json'
