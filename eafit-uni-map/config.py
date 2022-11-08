c99498817eab0c434dc02d503de4dc9f7c269389
Luis M. Torres-Villegas <111613017+LuisForPresident@users.noreply.github.com>
2022-11-07 22:44:36 -0500

    rename `time_and_distance.json` to `stats.json`

--- eafit-uni-map/config.py
+++ eafit-uni-map/config.py
@@ -1,3 +1,3 @@
 GRAPH_PATH = 'eafit-uni-map/data/eafit-campus.edgelist'
 FAVORITES_PATH = 'eafit-uni-map/data/favorites.json'
-STATS_PATH = 'eafit-uni-map/data/time_and_distance.json'
+STATS_PATH = 'eafit-uni-map/data/stats.json'
