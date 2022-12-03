a8c4684702444be806d2d794a27c12c03868ed01
Luis M. Torres-Villegas <111613017+LuisForPresident@users.noreply.github.com>
2022-12-02 20:46:09 -0500

    fix file path string literals
    README.md says that the working directory
    must be ./eafit-uni-map.
    
    The previous state tried to access the
    path twice, e.g., path/path.

--- eafit-uni-map/config.py
+++ eafit-uni-map/config.py
@@ -2,6 +2,6 @@
 File path string literals.
 """
 
-GRAPH_PATH = "eafit-uni-map/data/eafit-campus.edgelist"
-FAVORITES_PATH = "eafit-uni-map/data/favorites.json"
-STATS_PATH = "eafit-uni-map/data/stats.json"
+GRAPH_PATH = "./data/eafit-campus.edgelist"
+FAVORITES_PATH = "./data/favorites.json"
+STATS_PATH = "./data/stats.json"
